c96e335568c6a4b18e39e379bd9cb711bfa81491
Ian Jennings <ian@meetjennings.com>
2024-02-06 12:40:02 -0600

    add code location

--- client.js
+++ client.js
@@ -15,7 +15,7 @@ ipc.connectTo("world", function () {
   ipc.of.world.on("connect", function () {
     let text =
       process.argv[2] +
-      ', use chrome if you need to use a web browser. Once the process is done, make a file at /tmp/td.log. In this file, say either "The test failed" or "The test passed," then explain how you came to that conclusion and the workarounds you tried.';
+      '. Prefer to use Google Chrome if you need to use a web browser, unless otherwise instructed. If instructed to run code, the code is located at /Users/ec2-user/actions-runner/_work/testdriver/testdriver. Once the process is done, make a file at /tmp/td.log. In this file, say either "The test failed" or "The test passed," then explain how you came to that conclusion and the workarounds you tried.';
 
     text = text
       .replace("@testdriverai", "")

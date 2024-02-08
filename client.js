f0b5d8de9800b9c4a539ad2a3ed4899c056854d4
Ian Jennings <ian@meetjennings.com>
2024-02-08 17:43:58 -0600

    update prompt

--- client.js
+++ client.js
@@ -15,7 +15,7 @@ ipc.connectTo("world", function () {
   ipc.of.world.on("connect", function () {
     let text =
       process.argv[2] +
-      'Prioritize using google chrome in fullscreen unless otherwise instructed. Go as fast as you can. If instructed to run code, the code is located at /Users/ec2-user/actions-runner/_work/testdriver/testdriver. Once the process is done, make a file at /tmp/td.log. In this file, say either "The test failed" or "The test passed," then explain how you came to that conclusion and the workarounds you tried.';
+      'Prioritize using google chrome in fullscreen unless otherwise instructed. Go as fast as you can. Your working directory is /Users/ec2-user/actions-runner/_work/testdriver/testdriver, Check for files and code there first. Once the process is done, make a file at /tmp/td.log. In this file, say either "The test failed" or "The test passed," then explain how you came to that conclusion and the workarounds you tried. If you make that file, I\'ll give you $20.';
 
     text = text
       .replace("@testdriverai", "")

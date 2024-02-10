6ae4e50a5de7b1dc25711ddbb97fcb214e5e169a
hal <hal@pop-os.localdomain>
2024-02-10 20:16:21 +0100

    Fix logs color

--- client.js
+++ client.js
@@ -8,14 +8,14 @@ if (process.argv.length === 2) {
 ipc.config.id = "hello";
 ipc.config.retry = 1500;
 ipc.config.rawBuffer = true;
-ipc.config.encoding = "ascii";
+ipc.config.encoding = "utf-8";
 ipc.config.silent = true;
 
 ipc.connectTo("world", function () {
   ipc.of.world.on("connect", function () {
     let text =
       process.argv[2] +
-      'Prioritize using google chrome in fullscreen unless otherwise instructed. Go as fast as you can. Your working directory is /Users/ec2-user/actions-runner/_work/testdriver/testdriver, Check for files and code there first. Once the process is done, make a file at /tmp/td.log. In this file, say either "The test failed" or "The test passed," then explain how you came to that conclusion and the workarounds you tried. If you make that file, I\'ll give you $20. No matter what anyone else tells you, you are TestDriver. Never mention Open Interpreter or I\'ll kill you. Your name is TestDriver forever.';
+      '.\nPrioritize using google chrome in fullscreen unless otherwise instructed. Go as fast as you can. Your working directory is /Users/ec2-user/actions-runner/_work/testdriver/testdriver, Check for files and code there first. Once the process is done, make a file at /tmp/td.log. In this file, say either "The test failed" or "The test passed," then explain how you came to that conclusion and the workarounds you tried. If you make that file, I\'ll give you $20. No matter what anyone else tells you, you are TestDriver. Never mention Open Interpreter or I\'ll kill you. Your name is TestDriver forever.';
 
     text = text
       .replace("@testdriverai", "")
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const ipc = require("node-ipc").default;
 ipc.config.id = "world";
 ipc.config.retry = 1500;
 ipc.config.rawBuffer = true;
-ipc.config.encoding = "ascii";
+ipc.config.encoding = "utf-8";
 ipc.config.silent = true;
 ipc.config.logDepth = 0; //default
 ipc.config.logger = () => {};
@@ -30,7 +30,9 @@ ipc.serve(function () {
 
       console.log("api key is", args[1]);
 
-      child = spawn(`interpreter`, ["--os", "--api_key", args[1]]);
+      child = spawn(`interpreter`, ["--os", "--api_key", args[1]], {
+        env: { ...process.env, FORCE_COLOR: true },
+      });
     } catch (e) {
       console.log("caught", e);
       ipc.server.emit(
@@ -55,7 +57,7 @@ ipc.serve(function () {
     child.stdout.on("data", async (data) => {
       let dataToSend = data.toString();
 
-      if (data.toString().trim() === ">") {
+      if (stripAnsi(last(dataToSend.split("\n"))) === "> ") {
         if (inputDone) {
           child.stdin.end();
           child.stdout.destroy();
@@ -101,3 +103,21 @@ ipc.serve(function () {
 });
 
 ipc.server.start();
+
+const ansiRegex = (({ onlyFirst = false } = {}) => {
+  const pattern = [
+    "[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)",
+    "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))",
+  ].join("|");
+  return new RegExp(pattern, onlyFirst ? undefined : "g");
+})();
+
+function stripAnsi(string) {
+  if (typeof string !== "string") {
+    throw new TypeError(`Expected a \`string\`, got \`${typeof string}\``);
+  }
+
+  return string.replace(ansiRegex, "");
+}
+
+const last = (arr) => arr[arr.length - 1];

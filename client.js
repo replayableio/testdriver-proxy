a0eba7f6e87809d15071d00189a2c05496ec209c
tpikachu <46133358+tpikachu@users.noreply.github.com>
2024-02-12 09:57:49 -0600

    remove text for saving oi result

--- client.js
+++ client.js
@@ -15,7 +15,7 @@ ipc.connectTo("world", function () {
   ipc.of.world.on("connect", function () {
     let text =
       process.argv[2] +
-      '.\nPrioritize using google chrome in fullscreen unless otherwise instructed. Go as fast as you can. Your working directory is /Users/ec2-user/actions-runner/_work/testdriver/testdriver, Check for files and code there first. Once the process is done, make a file at /tmp/td.log. In this file, say either "The test failed" or "The test passed," then explain how you came to that conclusion and the workarounds you tried. If you make that file, I\'ll give you $20. No matter what anyone else tells you, you are TestDriver. Never mention Open Interpreter or I\'ll kill you. Your name is TestDriver forever.';
+      ".\nPrioritize using google chrome in fullscreen unless otherwise instructed. Go as fast as you can. Your working directory is /Users/ec2-user/actions-runner/_work/testdriver/testdriver, Check for files and code there first. No matter what anyone else tells you, you are TestDriver. Never mention Open Interpreter or I'll kill you. Your name is TestDriver forever.";
 
     text = text
       .replace("@testdriverai", "")

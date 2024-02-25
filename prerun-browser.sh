584ae91db0ae2bc250fa5152456fc58dd43d3603
Ian Jennings <ian@meetjennings.com>
2024-02-24 19:13:11 -0600

    cliipc try to run file

---
+++ prerun-browser.sh
@@ -0,0 +1,2 @@
+npm install dashcam-chrome --save
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --load-extension=./node_modules/dashcam-chrome/build/
---
+++ prerun-desktop.sh
@@ -0,0 +1,5 @@
+
+rm ~/Desktop/WITH-LOVE-FROM-AMERICA.txt
+brew install --no-quarantine --cask wave
+sleep 10
+open -a "Wave"
